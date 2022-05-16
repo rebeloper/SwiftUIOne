74d67417bc74d3c77d17d2381e0cadedd4f6f965
Alex Nagy <therebeloper@gmail.com>
2022-05-16 11:16:54 +0300

    configure SwiftUIOne

---
+++ Sources/SwiftUIOne.xcframework/ios-arm64_x86_64-simulator/SwiftUIOne.framework/Headers/SwiftUIOne.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUIOne.h
+//  SwiftUIOne
+//
+//  Created by Alex Nagy on 16.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIOne.
+FOUNDATION_EXPORT double SwiftUIOneVersionNumber;
+
+//! Project version string for SwiftUIOne.
+FOUNDATION_EXPORT const unsigned char SwiftUIOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIOne/PublicHeader.h>
+
+
